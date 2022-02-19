03e197a418bd6dabfc9a28de2d2d6951ca6511a9
Jesica262 <yesiifer@hotmail.com>
2022-02-19 18:07:28 -0300

    ubicacion

--- app/src/main/java/com/example/myarbolito/FragmentPedirArbol.java
+++ app/src/main/java/com/example/myarbolito/FragmentPedirArbol.java
@@ -4,50 +4,101 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
-
 import android.Manifest;
-
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
-
 import android.os.Bundle;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class FragmentPedirArbol extends Fragment  {
 
-public FragmentPedirArbol(){}
+    private GoogleMap nMap;
+    public List<LatLng> puntos;
+    public List<MarkerOptions> marcadoresDeInteres;
+    public int contador=0;
+
+    public FragmentPedirArbol(){}
+
     private OnMapReadyCallback callback = new OnMapReadyCallback(){
 
         @Override
         public void onMapReady(@NonNull GoogleMap googleMap) {
-            LatLng sydney = new LatLng(-34, 151);
-            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-            //  getDataFromFirebase(googleMap);
 
-            if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
+            nMap = googleMap;
+
+            this.actualizarMapa();
+            ejecutarActualizacionMapa();
+
+        }
+
+        private void actualizarMapa() {
+
+            if(ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED)
+            {
+                ActivityCompat.requestPermissions(getActivity(), new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION}, 9999);
                 return;
-            }
+                }
+        }
+
+        public Boolean noTienePermisoAcceso()
+        {
+            return ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED;
+        }
+
+        @SuppressLint("MissingPermission")
+        public void ejecutarActualizacionMapa()
+        {
+            marcadoresDeInteres = new ArrayList<>();
+            puntos = new ArrayList<>();
+           // LatLng SANTA_FE = new LatLng(-31.6370, -60.7269);
+           // nMap.moveCamera(CameraUpdateFactory.newLatLng(SANTA_FE));
+           // nMap.moveCamera(CameraUpdateFactory.zoomTo(12.0f));
+            LatLng sydney = new LatLng(-34, 151);
+            nMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+            nMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            nMap.setTrafficEnabled(true);
+            nMap.setMyLocationEnabled(true);
+            nMap.getUiSettings().setZoomControlsEnabled(true);
+            nMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                @Override
+                public void onMapClick(@NonNull LatLng latLng) {
+                    puntos.add(latLng);
+                }
+            });
+            nMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+                @Override
+                public void onMapLongClick(@NonNull LatLng latLng) {
+                    MarkerOptions o = new MarkerOptions()
+                            .alpha(0.7f)
+                            .snippet("Hola"+(++contador))
+                            .draggable(true)
+                            .position(latLng)
+                            .title("NOMBRE"+contador)
+                            .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_arbol))
+                            .visible(true);
+                    marcadoresDeInteres.add(o);
+                    nMap.addMarker(o);
+                }
+            });
         }
     };
 
--- app/src/main/java/com/example/myarbolito/MapsFragment.java
+++ app/src/main/java/com/example/myarbolito/MapsFragment.java
@@ -7,53 +7,32 @@ import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
-import android.util.Base64;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-
-
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class MapsFragment extends Fragment {
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
-
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-
 
         @Override
         public void onMapReady(GoogleMap googleMap) {
             LatLng sydney = new LatLng(-34, 151);
             googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
             googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
             // getDataFromFirebase(googleMap);
 
             if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -66,8 +45,6 @@ public class MapsFragment extends Fragment {
                 // for ActivityCompat#requestPermissions for more details.
                 return;
             }
-            // googleMap.setMyLocationEnabled(true);
-
         }
     };
 
